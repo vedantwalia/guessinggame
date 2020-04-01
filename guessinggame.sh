7fe8a1f83909f32cf0d1b2b736d80f9f11da387b
Vedant Walia <vedantwalia@outlook.com>
2020-04-01 10:43:19 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,4 @@
-count = 0
-
+count=0
 function data {
   local files = $(find . -maxdepth 1 -type f | wc -l | egrep -o "[0-9]+")
   echo files
@@ -15,16 +14,9 @@ function compare {
   else
       echo "Guess too high, try again!"
   fi
-
-echo "Please guess the number of files in the current directory."
-
-while [[ $count -eq 0 ]]; do
+while [[ $guess -ne $data ]]; do
+  echo "Please guess the number of files in the current directory."
   read guess
-  if [[ $(echo guess) | egrep -o "[^0-9]+" ]]; then
-        echo "Please emter your guess"
-        read guess
-    fi
-    compare
-  done
-
+  compare
+done
 }
